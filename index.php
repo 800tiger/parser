d5b14dd404c6126419c9c0a775366e8384b5ecb1
wu di <wudi.cladyata@gmail.com>
2021-06-15 17:34:06 +1000

    Modify the index page

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!--    
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSV Object Importer Task</title>
 </head>
 <body>
@@ -15,3 +27,4 @@
     </div>
 </body>
 </html>
+-->
