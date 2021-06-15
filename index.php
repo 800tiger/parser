7458061ea30ae2a12a1e8ccf4c2bfe7a99a3a5b2
wu di <wudi.cladyata@gmail.com>
2021-06-15 21:06:50 +1000

    format Fix language hack

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "CSV OBJECT PRASER" ?></title>
 </head>
+
 <body>
     <div>
         <form action="display.php" method="POST" enctype="multipart/form-data">
             <input id="csv_task" name="csv_task" type="file" />
-            <input type="submit" name="submit" value="Upload CSV"/> 
-        </form> 
+            <input type="submit" name="submit" value="Upload CSV" />
+        </form>
     </div>
 </body>
-</html>
 
+</html>
