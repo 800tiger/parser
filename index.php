58619f0a615ab5299878fcdb4546eaf090b4f28c
wu di <wudi.cladyata@gmail.com>
2021-06-15 17:36:45 +1000

    add header

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<!--    
-</body>
 </html>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<header>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSV Object Importer Task</title>
-</head>
+</header>
 <body>
     <div>
         <form action="display.php" method="POST" enctype="multipart/form-data">
@@ -27,4 +16,3 @@
     </div>
 </body>
 </html>
--->
